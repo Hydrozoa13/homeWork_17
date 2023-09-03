bd527f19ebf9dc845f810a18d28d4b2c3c006277
Zhenya Loyko <e.lojko@inbox.ru>
2023-09-03 19:52:44 +0300

    First part done

--- homeWork_17.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ homeWork_17.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -57,7 +57,7 @@ struct RandomGenerator: RandomNumberGenerator {
     }
 }
 
-var test3 = RandomGenerator.init()
+var test3 = RandomGenerator()
 test3.randomCollection
 test3.getRandomNumber()
 test3.setNewRandomCollection(newValue: [6,7,8,9,10])
@@ -77,7 +77,7 @@ class RandomGeneratorClass: RandomNumberGenerator {
     }
 }
 
-var test4 = RandomGeneratorClass.init()
+var test4 = RandomGeneratorClass()
 test4.randomCollection
 test4.getRandomNumber()
 test4.setNewRandomCollection(newValue: [16,17,18,19,20])
@@ -86,7 +86,123 @@ test4.getRandomNumber()
 
 //MARK: -
 
+protocol Named {
+    init(name: String)
+}
 
+class Person: Named {
+    required init(name: String) {
+        self.name = name
+    }
+    var name: String
+}
 
+let test5 = Person(name: "Evgeny")
+test5.name
+
+//MARK: -
+
+func getHash<T: Hashable>(of value: T) -> Int {
+    return value.hashValue
+}
+
+getHash(of: 6)
+getHash(of: "Hello")
+getHash(of: true)
+getHash(of: "A")
+
+//MARK: -
+
+protocol HasValue {
+    var value: Int { get set }
+}
+
+class ClassWithValue: HasValue {
+    var value: Int
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+struct StructWithValue: HasValue {
+    var value: Int
+}
+
+let objects: [Any] = [
+    2,
+    StructWithValue(value: 6),
+    true,
+    ClassWithValue(value: 21),
+    "Evgeny"
+]
+
+for object in objects {
+    if let elementWithValue = object as? HasValue {
+        print("Значение \(elementWithValue.value)")
+    }
+}
+
+for object in objects {
+    print(object is HasValue)
+}
+
+//MARK: -
+
+protocol GeometricFigureWithXAxis {
+    var x: Int { get set }
+}
+
+protocol GeometricFigureWithYAxis {
+    var y: Int { get set }
+}
+
+protocol GeometricFigureTwoAxis: GeometricFigureWithXAxis,
+                                     GeometricFigureWithYAxis {
+    var distanceFromCenter: Float { get }
+}
+
+struct Figure2D: GeometricFigureTwoAxis {
+    var x: Int = 0
+    var y: Int = 0
+    var distanceFromCenter: Float {
+        let xPow = pow(Double(self.x), 2)
+        let yPow = pow(Double(self.y), 2)
+        let length = sqrt(xPow + yPow)
+        return Float(length)
+    }
+}
+
+let test6 = Figure2D(x: 4, y: 3)
+test6.distanceFromCenter
+
+//MARK: -
+
+protocol SuperProtocol {}
+protocol SubProtocol: class, SuperProtocol {}
+
+class ClassWithProtocol: SubProtocol {}
+//struct StructWithProtocol: SubProtocol {}
+
+//MARK: -
+
+protocol Name {
+    var name: String { get }
+}
+
+protocol Age {
+    var age: Int { get }
+}
+
+struct Persona: Name, Age {
+    var name: String
+    var age: Int
+}
+
+func wishHappyBirthday(celebrator: Name & Age) {
+    print("С Днем Рождения \(celebrator.name)! Тебе уже \(celebrator.age)!")
+}
+
+let person = Persona(name: "Evgeny", age: 23)
+wishHappyBirthday(celebrator: person)
 
 //: [Next](@next)
